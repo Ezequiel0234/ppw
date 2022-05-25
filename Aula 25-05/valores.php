690fb9ac8092084ef32f101e8de353e672163e6f
Ezequiel0234 <106176911+Ezequiel0234@users.noreply.github.com>
2022-05-25 08:43:14 -0500

    Add files via upload

---
+++ Aula 25-05/valores.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valores</title>
+</head>
+<body>
+    <?php
+
+    $v1 = $_GET['valor1'];
+    $v2 = $_GET['valor2'];
+    $v3 = $_GET['valor3'];
+    $vs = $_GET['valores'];
+
+    //echo "Valor 1: $v1\nValor 2: $v2\nValor"
+
+    ?>
+</body>
+</html>
